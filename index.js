ff117e13fb3302bd6bf49f6272cd8dd3e6c60045
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-08-18 20:59:32 +0530

    Initialized Mongodb connection

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ app.get('/', (req, res) => {
 app.use(cors());
 app.use(express.json());
 
+//mongodb connection
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:admin_book_store@cluster0.uvlqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
