1e381203e600b22c55ef125b2f114b3c5174de0e
yashksaini-coder <ys3853428@gmail.com>
2024-08-26 20:30:11 +0530

    Add Shop section

---
+++ frontend/src/shop/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <h1>Shop Page</h1>
+            {/* Add your shop content here */}
+        </div>
+    );
+};
+
+export default Shop;
