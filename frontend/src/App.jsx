fff0ca643d73ded0f1316ffb9cd372aef05bcb9e
yashksaini-coder <ys3853428@gmail.com>
2024-08-26 20:51:54 +0530

    Created Simple Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
+import React from 'react'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+      <Outlet/>
+    </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {RouterProvider} from 'react-router-dom'
+import router from './routers/router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
