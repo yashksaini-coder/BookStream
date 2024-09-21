9c0b07c518ae24d8e3e8fd0e766181a8487825ec
HYnam <hiuyi.nam@uqconnect.edu.au>
2024-09-21 18:26:55 +0100

    set up footer navbar and home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
+import Home from "./pages/Home";
+import Navbar from './component/Navbar/Navbar';
+import Footer from './component/Footer/Footer';
 
-function App() {
+const App = () => {
 
   return (
-    <>
-      Test Home Page
-    </>
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
   )
 }
 
